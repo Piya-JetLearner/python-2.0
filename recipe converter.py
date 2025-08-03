b30b289c9ca27eb8c09632739db7373cab593d58
Piya-JetLearner <piyajhp@gmail.com>
2025-08-03 10:08:52 +0100

    Update recipe converter.py

--- recipe converter.py
+++ recipe converter.py
@@ -18,4 +18,20 @@ while True:
     if rc==1:
         c=int(input("how many cups do you want to convert into grams?"))
         g=c_g(c)
+
         print(g,'grams')
+        elif rc==2:
+        g=int(input("How many grams do you want to convert into cups? "))
+        grams=g_c(g) 
+        print(grams,'cups')
+    elif rc==3:
+        te=int(input("How many teaspoons do you want to convert into tablespoons? "))
+        teaspoons=te_ta(te) 
+        print(teaspoons,'tablespoons')
+    elif rc==4:
+        ta=int(input("How many tablespoons do you want to convert into teaspoons? "))
+        tablespoons=ta_te(ta) 
+        print(tablespoons,'teaspoons')
+    elif rc==5:
+        print("thank you for viewing my recipe converter! Goodbye.")
+        break 
