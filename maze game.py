5c7300477f91ba4a72491717c4f9c81a76e75faf
Piya-JetLearner <piyajhp@gmail.com>
2025-10-03 19:29:18 +0100

    Update maze game.py

--- maze game.py
+++ maze game.py
@@ -34,7 +34,9 @@ player.speed(0)
 
 #Obstacle setup 
 obstacles=[]
+finish=''
 def create ():
+    global finish
     for y in range(len(maze)):
         for x in range(len(maze[y])):
             character= maze[y][x]
@@ -53,39 +55,50 @@ def create ():
                 finish.color("green")
                 finish.penup()
                 finish.goto(screen_x, screen_y)
+create()  
 def is_valid_move(x,y):
     for obstacle in obstacles:
         if obstacle.xcor()==x and obstacle.ycor()==y:
             return False
     return True
+def checkpoint():
+    global finish
+    if player.distance(finish)<10:
+        print("you won!")
+        screen.bye()
 def move_up():
     new_x= player.xcor()
     new_y = player.ycor()+24
     if is_valid_move(new_x, new_y):
         player.goto(new_x, new_y)
+        checkpoint()
 def move_down():
     new_x= player.xcor()
     new_y = player.ycor()-24
     if is_valid_move(new_x, new_y):
         player.goto(new_x, new_y)
+        checkpoint()
 def move_left():
     new_x= player.xcor()-24
     new_y = player.ycor()
     if is_valid_move(new_x, new_y):
         player.goto(new_x, new_y)
+        checkpoint()
 def move_right():
     new_x= player.xcor()+24
     new_y = player.ycor()
     if is_valid_move(new_x, new_y):
         player.goto(new_x, new_y)
+        checkpoint()
 screen.listen()
 screen.onkey(move_up,"Up")
 screen.onkey(move_down,"Down")
 screen.onkey(move_left, "Left")
 screen.onkey(move_right, "Right")
-create()
+
 player.goto(-264,264)
 screen.mainloop()
+
 
 
 
