53c3c63bef00b7c28cd82ef885ce79983227bad3
Piya-JetLearner <piyajhp@gmail.com>
2025-10-24 20:03:37 +0100

    Add files via upload

---
+++ capstone project.py
@@ -0,0 +1,7 @@
+import turtle
+screen=turtle.Screen()
+screen.bgpic("game background.gif")
+screen.addshape("resized person icon.gif")
+p=turtle.Turtle()
+p.shape("resized person icon.gif")
+screen.mainloop()
